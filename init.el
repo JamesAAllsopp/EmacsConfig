bd210f0ca4e8e616596e5ddae4b493dcbc3aaae8
James Allsopp <j.allsopp@bham.ac.uk>
2023-04-16 23:13:34 +0100

    Adding Centaur-tabs, used default website configuration

--- init.el
+++ init.el
@@ -22,8 +22,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-agenda-files '("~/.org/home_jobs.org"))
  '(package-selected-packages
-   '(magit ein numpydoc flymake-ruff blacken python-pytest pyvenv modus-themes helpful which-key use-package spacegray-theme org-superstar org-roam-bibtex org-ref org-appear olivetti mixed-pitch helm-bibtex deft company-posframe)))
+   '(centaur-tabs magit ein numpydoc flymake-ruff blacken python-pytest pyvenv modus-themes helpful which-key use-package spacegray-theme org-superstar org-roam-bibtex org-ref org-appear olivetti mixed-pitch helm-bibtex deft company-posframe)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -145,3 +146,4 @@
 (load "~/.emacs.d/settings/python-settings.el")
 (load "~/.emacs.d/settings/font-settings.el")
 (load "~/.emacs.d/settings/backups-settings.el")
+(load "~/.emacs.d/settings/centaur-tabs.el")
