fbb42e0cfc9d8290bc7cf311053769f0e58bf90b
James Allsopp <j.allsopp@bham.ac.uk>
2023-09-23 15:23:11 +0100

    Added Tramp.el from neil's repository
    Here's a file to hopefully fix the tramp problem.

---
+++ settings/tramp.el
@@ -0,0 +1,18 @@
+(use-package tramp
+  :ensure t
+  :config
+  (setq tramp-default-method "ssh")
+  (add-to-list 'tramp-default-method-alist '("" "neil" "ssh"))
+  ;; Set prompt so it doesn't hang
+  (setq shell-prompt-pattern '"^[^#$%>\n]*~?[#$%>] *")
+  ;; Set default usernames for different hosts and a global default.
+  ;; (add-to-list 'tramp-default-user-alist
+  ;;          '("ssh" ".*ovh'" "arch") t)
+  ;; (add-to-list 'tramp-default-user-alist
+  ;;          '("ssh" ".*openwrt" "admin") t)
+  ;; (add-to-list 'tramp-default-user-alist
+  ;;          '(nil nil "neil") t)
+  ;; (add-to-list 'tramp-default-user-alist
+  ;;          '("ssh" ".*alarmpi-4b" "neil") t)
+  )
+
