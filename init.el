62ec2b67078e43c35e0f6fb29bfa9e99d3afc8a8
James Allsopp <j.allsopp@bham.ac.uk>
2023-10-13 14:36:06 +0100

    Removed leaky merge line

--- init.el
+++ init.el
@@ -36,7 +36,6 @@
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
->>>>>>> e9bbec3 (Changing agenda directory to use a repository)
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
