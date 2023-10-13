b0e79b701119fa187cc38b4fd1af4c22fc34bc28
James Allsopp <jamesaallsopp@googlemail.com>
2023-10-13 11:55:10 +0100

    Changing agenda directory to use a repository

--- init.el
+++ init.el
@@ -30,15 +30,18 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files '("~/.org/home_jobs.org"))
+ '(org-agenda-files '("~/org-agenda/"))
  '(package-selected-packages
    '(tramp yaml-mode org-roam org-roam-ui nlinum centaur-tabs magit ein numpydoc flymake-ruff blacken python-pytest pyvenv modus-themes helpful which-key use-package spacegray-theme org-superstar org-roam-bibtex org-ref org-appear olivetti mixed-pitch helm-bibtex deft company-posframe)))
 
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+>>>>>>> e9bbec3 (Changing agenda directory to use a repository)
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ ;;'(SOME-OPTION THE-OPTION-VALUE)
+  ;; ...
  )
 
 
@@ -154,4 +157,3 @@
 (load "~/.emacs.d/settings/org-roam.el")
 (load "~/.emacs.d/settings/mermaid-mode.el")
 (load "~/.emacs.d/settings/numpydoc.el")
-
